7571ef17cc1628c9b04a70ea16bb2c20b3b8d095
Liz McCain <mccain442@gmail.com>
2021-08-13 14:25:48 -0700

    functions to generate random characters created

--- Develop/script.js
+++ Develop/script.js
@@ -1,17 +1,32 @@
 // Assignment code here
 
+//Generator functions
+function getRandomLower(){
+  return String.fromCharCode(Math.floor(Math.random() * 26) + 97);
+}
+function getRandomUpper(){
+  return String.fromCharCode(Math.floor(Math.random() * 26) + 65);
+}
+function getRandomNumber(){
+  return String.fromCharCode(Math.floor(Math.random() * 10) + 48);
+}
+function getRandomSymbol(){
+  return String.fromCharCode(Math.floor(Math.random() * 14) + 33);
+}
+
+
 
 // Get references to the #generate element
 var generateBtn = document.querySelector("#generate");
 
 // Write password to the #password input
-function writePassword() {
-  var password = generatePassword();
-  var passwordText = document.querySelector("#password");
+// function writePassword() {
+  //var password = generatePassword();
+  //var passwordText = document.querySelector("#password");
 
-  passwordText.value = password;
+  //passwordText.value = password;
 
-}
+//}
 
 // Add event listener to generate button
-generateBtn.addEventListener("click", writePassword);
+generateBtn.addEventListener("click", writePassword); 
