681639bed477095d817ba291bd64929b2f442e5f
Liz McCain <mccain442@gmail.com>
2021-08-20 19:14:12 -0700

    extra funtions and random names deleted

--- Develop/script.js
+++ Develop/script.js
@@ -7,7 +7,7 @@ var confirmLower;
 var confirmLength;
 
 //This is where the password will be stored as it is created
-var password ="";
+var password = "";
 //var values
 //lowerCase string
 var lowerCase = 'abcdefghijklmnopqrstuvwxyz';
@@ -18,62 +18,32 @@ var number = '0123456789';
 //symbol string
 var symbol = '!@#$%^&*()<~?:+';
 //This is where th input will be stored I think...
-var length ="";
-
-
-//button function to start promts 
-var generateBtn = document.querySelector("#generate").addEventListener('click', generate);
-//generates the password
-function generate(){
-  userprompt();
-  //generatePassword();
-} 
-
+var length = "";
 
 //Generator functions
-var userprompt = function() {
+var generatePassword = function () {
   //ask for input
   var confirmLength = parseInt(prompt("How many characters would you like your password to be? Enter a number between 8-128"));
   //if statement to insure proper length  
-  if (!confirmLength) { 
-    window.alert("Enter a number between 8-128"); 
-    return userprompt();
+  if (!confirmLength) {
+    window.alert("Enter a number between 8-128");
+    return generatePassword();
   }
-  else if (confirmLength <8 || confirmLength >128) {
-    window.alert ("Number must be betweern 8-128");
-    return userprompt();
+  else if (confirmLength < 8 || confirmLength > 128) {
+    window.alert("Number must be betweern 8-128");
+    return generatePassword();
   }
   else {
     //continue through promts after valid answer
-  confirmLower = window.confirm("Would you like lowercase letters in your password? 'OK' for yes, 'Cancel for no");
-  confirmUpper = window.confirm("Would you like uppercase letters in your password? 'OK' for yes, 'Cancel for no");
-  confirmNum = window.confirm("Would you like numbers in your password? 'Ok' for yes, 'Cancel' for no");
-  confirmChar = window.confirm("Would you like special characters in your password? 'OK' for yes, 'Cancel for no.");
+    confirmLower = window.confirm("Would you like lowercase letters in your password? 'OK' for yes, 'Cancel for no");
+    confirmUpper = window.confirm("Would you like uppercase letters in your password? 'OK' for yes, 'Cancel for no");
+    confirmNum = window.confirm("Would you like numbers in your password? 'Ok' for yes, 'Cancel' for no");
+    confirmChar = window.confirm("Would you like special characters in your password? 'OK' for yes, 'Cancel for no.");
   };
 }
 
-
-
-// Write password to the #password input
- function writePassword() {
-   if (confirmNum) {
-     console.log(writePassword)
-     password.push(number)
-   } else {
-     
-   }
-   if (condition) {
-     
-   } else {
-     
-   }
- var password = generatePassword();
- var passwordText = document.querySelector("#password");
-
- passwordText.value = password;
-
+function generate() {
+generatePassword();
 }
-
-
 //button function to start promts 
 var generateBtn = document.querySelector("#generate").addEventListener('click', generate);
