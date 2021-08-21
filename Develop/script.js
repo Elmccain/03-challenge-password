fc8665fb202c5929f6f60077afde1b875c7cc72e
Liz McCain <mccain442@gmail.com>
2021-08-21 16:52:45 -0700

    final final

--- Develop/script.js
+++ Develop/script.js
@@ -4,10 +4,10 @@ var upperCase = 'ABCDEFGHIJKLMNOPQRSTUVWXYZ';
 var number = '0123456789';
 var symbol = '!@#$%^&*()<~?:+';
 //Generator functions -- Office hours helped me cut out all of the if statements I was planning to write
- var generatePassword = function () {
-password = "";
-pwLength = "";
-var confirmLength = parseInt(prompt("How many characters would you like your password to be? Enter a number between 8-128"));
+var generatePassword = function () {
+  password = "";
+  pwLength = "";
+  var confirmLength = parseInt(prompt("How many characters would you like your password to be? Enter a number between 8-128"));
   //if statement to insure proper length  
   if (!confirmLength) {
     window.alert("Enter a number between 8-128");
@@ -17,36 +17,35 @@ var confirmLength = parseInt(prompt("How many characters would you like your pas
     window.alert("Number must be betweern 8-128");
     return generatePassword();
   }
-  else 
- //continue through promts after valid answer
- confirmLower = window.confirm("Would you like lowercase letters in your password? 'OK' for yes, 'Cancel for no");
- if (confirmLower) {
-pwLength = pwLength + lowerCase
-}
- confirmUpper = window.confirm("Would you like uppercase letters in your password? 'OK' for yes, 'Cancel for no");
- if (confirmUpper) {
-pwLength = pwLength + upperCase;
-}
- confirmNum = window.confirm("Would you like numbers in your password? 'Ok' for yes, 'Cancel' for no");
- if (confirmNum) {
-pwLength = pwLength + number;
-}
- confirmChar = window.confirm("Would you like special characters in your password? 'OK' for yes, 'Cancel for no.");
- if (confirmChar) {
-pwLength = pwLength + symbol;
-}  
-//if all false circle back
-if (!confirmChar && !confirmLower && !confirmNum && !confirmUpper) {
-  alert("Give me something to work with!");
-  return generatePassword();
-}
-console.log(pwLength);
-for (let i = 0; i < confirmLength; i++) {
-  var password = password + pwLength.charAt(Math.floor(Math.random() * pwLength.length))
-  console.log(password);
-}
-//Display the result
-document.getElementById("password").innerHTML = password;
+  else
+    //continue through promts after valid answer
+    confirmLower = window.confirm("Would you like lowercase letters in your password? 'OK' for yes, 'Cancel for no");
+  if (confirmLower) {
+    pwLength = pwLength + lowerCase
+  }
+  confirmUpper = window.confirm("Would you like uppercase letters in your password? 'OK' for yes, 'Cancel for no");
+  if (confirmUpper) {
+    pwLength = pwLength + upperCase;
+  }
+  confirmNum = window.confirm("Would you like numbers in your password? 'Ok' for yes, 'Cancel' for no");
+  if (confirmNum) {
+    pwLength = pwLength + number;
+  }
+  confirmChar = window.confirm("Would you like special characters in your password? 'OK' for yes, 'Cancel for no.");
+  if (confirmChar) {
+    pwLength = pwLength + symbol;
+  }
+  //if all false circle back
+  if (!confirmChar && !confirmLower && !confirmNum && !confirmUpper) {
+    alert("Give me something to work with!");
+    return generatePassword();
+  }
+  console.log(pwLength);
+  for (let i = 0; i < confirmLength; i++) {
+    var password = password + pwLength.charAt(Math.floor(Math.random() * pwLength.length));
+  }
+  //Display the result
+  document.getElementById("password").innerHTML = password;
 };
 
 generatePassword();
